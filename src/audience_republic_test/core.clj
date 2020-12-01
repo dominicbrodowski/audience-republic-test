55a59dee4e36bd5c0d298d3675e5afa654d1f66f
dominic <dominic.brodowski@gmail.com>
2020-12-02 00:20:20 +1100

    Fixed eccentricity implementation to be correct, added tests for eccentricity, radius and diameter

--- src/audience_republic_test/core.clj
+++ src/audience_republic_test/core.clj
@@ -41,7 +41,9 @@
   [num-vertices]
   (let [integer-range (range 1 num-vertices)
         minimal-graph (reduce
-                       (fn [coll key] (assoc coll (int-to-key key) {(int-to-key (inc key)) (inc (rand-int 10))})) {} integer-range)]
+                       (fn [coll key]
+                         (assoc coll (int-to-key key) {(int-to-key (inc key)) (inc (rand-int 10))}))
+                       {} integer-range)]
     (assoc minimal-graph (int-to-key num-vertices) {})))
 
 
@@ -59,24 +61,31 @@
   (let [integer-range (range 1 (inc (count keys)))
         string-range  (map str integer-range)
         keyword-range (map keyword string-range)]
-    (remove #(= (first %) (second %)) (apply concat (reduce (fn [coll key] (conj coll (map list (repeat key) keyword-range))) [] keys)))))
+    (remove #(= (first %) (second %))
+            (apply concat
+                   (reduce (fn [coll key] (conj coll (map list (repeat key) keyword-range))) [] keys)))))
 
 
 (defn generate-edges
   "Will attempt to generate the num-edges amount of edges, including the minimal set."
   [graph num-vertices num-edges]
-  (let [current-graph   (construct-minimal-graph num-vertices)
-        remaining-edges (inc (- num-edges num-vertices))
-        edge-possibilities (generate-all-edge-possibilities (keys graph))
-        edge-possibilities-without-minimal-graph (remove (fn [x] (let [key-int (key-to-int (first x))
-                                                                       val-int (key-to-int (second x))]
-                                                                   (= key-int (dec val-int)))) edge-possibilities)
-        shuffled-edges (shuffle edge-possibilities-without-minimal-graph)
-        num-intended-edges (take remaining-edges shuffled-edges)]
-    (reduce (fn [coll x]
-              (let [_key (first x)
-                    _val (second x)]
-                (update-in coll [_key] assoc _val (inc (rand-int 10))))) current-graph num-intended-edges)))
+  (let [current-graph                            (construct-minimal-graph num-vertices)
+        remaining-edges                          (inc (- num-edges num-vertices))
+        edge-possibilities                       (generate-all-edge-possibilities (keys graph))
+        edge-possibilities-without-minimal-graph (remove
+                                                   (fn [x]
+                                                     (let [key-int (key-to-int (first x))
+                                                           val-int (key-to-int (second x))]
+                                                       (= key-int (dec val-int))))
+                                                   edge-possibilities)
+        shuffled-edges                           (shuffle edge-possibilities-without-minimal-graph)
+        num-intended-edges                       (take remaining-edges shuffled-edges)]
+    (reduce
+      (fn [coll x]
+        (let [_key (first x)
+              _val (second x)]
+          (update-in coll [_key] assoc _val (inc (rand-int 10)))))
+      current-graph num-intended-edges)))
 
 
 (defn generate-graph
@@ -102,7 +111,14 @@
 
 (defn- dijkstra-node
   "Recursive function that calculates shortest path via Dijkstra's algorithm."
-  [graph source destination unvisited-set visited-set node-id neighbours distances]
+  [graph
+   source
+   destination
+   unvisited-set
+   visited-set
+   node-id
+   neighbours
+   distances]
   (let [node-distance                 (get-in distances [node-id :distance])
         node-path                     (get-in distances [node-id :path])
         new-path                      (conj node-path node-id)
@@ -146,54 +162,29 @@
         distances            (update-in raw-distances [node-id] assoc :distance 0 :path [])]
     ; set all distances to infinity, except for the source which is zero
     (let [dijkstra-map     (dijkstra-node graph source destination unvisited-set visited-set node-id unvisited-neighbours distances)]
-      (get-in dijkstra-map [destination :path]))))
+      (get-in dijkstra-map [destination]))))
 
 
-(defn- eccentricity-node
-  "Reverse of Dijkstra's algorithm."
-  [graph source unvisited-set visited-set node-id neighbours distances]
-  (let [node-distance                 (get-in distances [node-id :distance])
-        node-path                     (get-in distances [node-id :path])
-        new-path                      (conj node-path node-id)
-        ; 2. Check ALL neighbours and calculate their distances via this node
-        unvisited-neighbours          (remove (fn [neighbour] (contains? (set visited-set) (first neighbour))) neighbours)
-        neighbour-tentative-distances (reduce (fn [coll v] (assoc coll (first v) (+ node-distance (second v)))) {} unvisited-neighbours)
-        ; 3. For each neighbour, update the distance and path if it is larger than the current path
-        updated-distances             (reduce
-                                       (fn [coll v]
-                                         (let [current-key           (first v)
-                                               current-record-holder (get-in distances [current-key :distance])
-                                               current-contender     (neighbour-tentative-distances current-key)]
-                                           (if (and (not (nil? current-contender)) (> current-contender current-record-holder))
-                                             ; update the distance
-                                             (assoc coll current-key {:distance current-contender :path new-path})
-                                             ; otherwise leave as is
-                                             (assoc coll current-key (distances current-key)))))
-                                       {} distances)
-        ; 4. Then set the current node to visited, and remove it from the unvisited set
-        updated-visited-set           (conj visited-set node-id)
-        updated-unvisited-set         (remove #{node-id} unvisited-set)]
-    ; 5. IF the destination node is visited, then stop.
-    (if (or (empty? updated-unvisited-set) (empty? (set/union updated-unvisited-set (first unvisited-neighbours))))
-      updated-distances
-      ; 6. ELSE select the unvisited node with the smallest tentative distance
-      (let [distance-map (reduce (fn [coll v] (assoc coll v (get-in updated-distances [v :distance]))) {} updated-unvisited-set)
-            largest-key (reduce (fn [init v] (if (> (second v) (second init)) v init)) distance-map)
-            new-key      (first largest-key)]
-        (eccentricity-node graph source updated-unvisited-set updated-visited-set new-key (graph new-key) updated-distances)))))
+(deftest dijkstra-test
+  (let [graph {:1 {:2 1 :3 10 :4 10 :5 10}, :2 {:3 3, :1 1}, :3 {:4 1, :1 1}, :4 {:5 1, :3 1, :2 1}, :5 {:1 3, :2 8}}
+        dijkstra (dijkstras-algorithm graph :1 :5)]
+    (is (= 6 (:distance dijkstra)))
+    (is (= [:1 :2 :3 :4] (:path dijkstra)))))
 
 
 (defn eccentricity
   "Longest path from source to any other vertex in the graph provided."
   [graph source]
-  (let [unvisited-set (keys graph)
-        visited-set []
-        node-id source
-        unvisited-neighbours (graph node-id)
-        distances (reduce (fn [coll key] (assoc coll key {:distance 0 :path []})) {} unvisited-set)]
-    (let [eccentricity-map (eccentricity-node graph source unvisited-set visited-set node-id unvisited-neighbours distances)
-          distances (map #(:distance (second %)) eccentricity-map)]
-      (apply max distances))))
+  (let [set-of-destinations (remove #{source} (keys graph))
+        eccentricity-map    (map #(dijkstras-algorithm graph source %) set-of-destinations)
+        distances           (map #(:distance %) eccentricity-map)]
+    (apply max distances)))
+
+
+(deftest eccentricity-test
+  (let [graph {:1 {:2 1 :4 1} :2 {:3 1} :3 {:4 1} :4 {:5 1} :5 {:1 1}}
+        eccentricity (eccentricity graph :1)]
+    (is (= 2 eccentricity))))
 
 
 (defn radius
@@ -206,6 +197,14 @@
   "The largest eccentricity found in the graph provided."
   [graph]
   (apply max (map #(eccentricity graph %) (keys graph))))
+
+
+(deftest radius-and-diameter-test
+  (let [graph {:1 {:2 5 :3 5 :4 5 :5 5} :2 {:1 6 :3 6 :4 6 :5 6} :3 {:1 4 :2 4 :4 4 :5 4} :4 {:1 5 :2 5 :3 5 :5 5} :5 {:1 5 :2 5 :3 5 :4 5}}
+        radius (radius graph)
+        diameter (diameter graph)]
+    (is (= 4 radius))
+    (is (= 6 diameter))))
 
 
 (defn seq-graph [initial-collection graph start-node]
